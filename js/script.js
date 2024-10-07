ca4dc5aa7a5ca570fb20eaa32846da286f8d1e93
Hiramaru-web12 <meg96.mcr2st@gmail.com>
2024-10-07 12:22:01 +0900

    JSの修正

--- js/script.js
+++ js/script.js
@@ -1,36 +1,27 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function() {
   // ドロワー開閉
- $('.c-drawer__icon').click(function(e){
-  e.preventDefault();
-  $('.c-drawer__icon, .l-drawer, .l-drawer__bg').toggleClass(`is-active`);
-  return false
- });
+  const drawerIcon = document.querySelector('.c-drawer__icon');
+  const drawer = document.querySelector('.l-drawer');
+  const drawerBg = document.querySelector('.l-drawer__bg');
 
- $('.l-drawer__link a[href^="#"]').click(function(e){
-  e.preventDefault();
-  $('.c-drawer__icon, .l-drawer, .l-drawer__bg').removeClass(`is-active`);
-  return false
- });
+  drawerIcon.addEventListener('click', function(e) {
+    e.preventDefault();
+    drawerIcon.classList.toggle('is-active');
+    drawer.classList.toggle('is-active');
+    drawerBg.classList.toggle('is-active');
+    return false;
+  });
 
- // #から始まるURLがクリックされた時
- $('a[href^="#"]').click(function() {
-  // 移動速度を指定（ミリ秒）
-  let speed = 500;
-  // hrefで指定されたidを取得
-  let id = $(this).attr('href');
-  // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
-  let target = $("#" == id ? "html" : id);
-  // ページのトップを基準にターゲットの位置を取得
-  let position = $(target).offset().top;
-  // ターゲットの位置までspeedの速度で移動
-  $('html, body').animate(
-    {
-      scrollTop: position - $('#js-header').outerHeight()
-   },
-    speed, "swing"
-  );
-  return false;
- });
-
+  const drawerLinks = document.querySelectorAll('.l-drawer__link a[href^="#"]');
+  drawerLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      drawerIcon.classList.remove('is-active');
+      drawer.classList.remove('is-active');
+      drawerBg.classList.remove('is-active');
+      return false;
+    });
+  });
 });
+
 
